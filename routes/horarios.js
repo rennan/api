4a8bf5c6f0d0c44d470745256f00066bbbcb3e4b
unknown <rennan.rodrigues@PC182.advise.local>
2015-09-14 08:39:02 -0300

    Added all get /horarios

--- routes/horarios.js
+++ routes/horarios.js
@@ -33,28 +33,116 @@ router.route('/via/:id_via')
 router.route('/via/:id_via/dias-uteis')
     // Obter informações de horários de uma via em dias úteis
     .get(function(req, res) {
-
+        var id_via = req.params.id_via;
+        req.getConnection(function(err, connection) {
+            var query = connection.query('SELECT * FROM horarios WHERE id_via = ? AND dias_uteis = 1', id_via, function(err, rows) {
+                if (err) {
+                    res.status(200).json({
+                        status: false,
+                        message: 'Erro desconhecido. Por favor tente novamente.'
+                    });
+                } else {
+                    if (rows.length > 0) {
+                        res.json({
+                            status: true,
+                            horarios: rows
+                        });
+                    } else {
+                        res.status(200).json({
+                            status: false,
+                            message: 'Não existe via com esse id.'
+                        });
+                    }
+                }
+            });
+        });
     });
 
 
 router.route('/via/:id_via/sabado')
     // Obter informações de horários de uma via aos sabados
     .get(function(req, res) {
-
+        var id_via = req.params.id_via;
+        req.getConnection(function(err, connection) {
+            var query = connection.query('SELECT * FROM horarios WHERE id_via = ? AND sabado = 1', id_via, function(err, rows) {
+                if (err) {
+                    res.status(200).json({
+                        status: false,
+                        message: 'Erro desconhecido. Por favor tente novamente.'
+                    });
+                } else {
+                    if (rows.length > 0) {
+                        res.json({
+                            status: true,
+                            horarios: rows
+                        });
+                    } else {
+                        res.status(200).json({
+                            status: false,
+                            message: 'Não existe via com esse id.'
+                        });
+                    }
+                }
+            });
+        });
     });
 
 
 router.route('/via/:id_via/domingo')
     // Obter informações de horários de uma via aos domingos
     .get(function(req, res) {
-
+        var id_via = req.params.id_via;
+        req.getConnection(function(err, connection) {
+            var query = connection.query('SELECT * FROM horarios WHERE id_via = ? AND domingo = 1', id_via, function(err, rows) {
+                if (err) {
+                    res.status(200).json({
+                        status: false,
+                        message: 'Erro desconhecido. Por favor tente novamente.'
+                    });
+                } else {
+                    if (rows.length > 0) {
+                        res.json({
+                            status: true,
+                            horarios: rows
+                        });
+                    } else {
+                        res.status(200).json({
+                            status: false,
+                            message: 'Não existe via com esse id.'
+                        });
+                    }
+                }
+            });
+        });
     });
 
 
 router.route('/via/:id_via/feriados')
     // Obter informações de horários de uma via aos feriados
     .get(function(req, res) {
-
+        var id_via = req.params.id_via;
+        req.getConnection(function(err, connection) {
+            var query = connection.query('SELECT * FROM horarios WHERE id_via = ? AND feriado = 1', id_via, function(err, rows) {
+                if (err) {
+                    res.status(200).json({
+                        status: false,
+                        message: 'Erro desconhecido. Por favor tente novamente.'
+                    });
+                } else {
+                    if (rows.length > 0) {
+                        res.json({
+                            status: true,
+                            horarios: rows
+                        });
+                    } else {
+                        res.status(200).json({
+                            status: false,
+                            message: 'Não existe via com esse id.'
+                        });
+                    }
+                }
+            });
+        });
     });
 
 
